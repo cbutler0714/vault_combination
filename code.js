9d5c5cdc746f48bc20d039fe97da00d416a92ffc
Christian Butler <christianbutler@MacBook-Pro.local>
2024-05-23 12:21:47 -0600

    first upload

---
+++ code.js
@@ -0,0 +1,17 @@
+const greeting = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+// Above is the initial message received 
+
+let num1 = 5 * 2
+
+let num2 = 13 + 27
+
+let num3 = 319 % 40
+
+// The alerts above correspond to the three numbers that will unlock the vault and their order
+
+alert(num1)
+alert(num2)
+alert(num3)
+
+// These alerts will display popup dialogs showing the numbers needed to unlock the vault
